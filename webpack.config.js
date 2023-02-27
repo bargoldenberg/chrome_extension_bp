af4399cc98a26a782d132fc3791e6f0825c3c8ec
bar.goldenberg <bar.goldenberg@similarweb.com>
2023-02-27 20:53:32 +0200

    boiler plate code

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { resolve } = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const tsRule = {
+  test: /\.ts(x?)$/,
+  exclude: /node_modules/,
+  use: "ts-loader",
+};
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "src/popup-page/popup.html",
+    filename: "popup.html",
+    chunks: ["popup"],
+  }),
+  new CopyWebpackPlugin({
+    patterns: [{ from: "public", to: "." }],
+  }),
+  new CleanWebpackPlugin(),
+];
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    popup: "./src/popup-page/popup.tsx",
+    contentscript: "./src/content.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [tsRule],
+  },
+  plugins,
+};
